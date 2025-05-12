3e09b018e631e8fda831ec2b639ba175491d8ddc
Francis Pham <francis.pham.ca@gmail.com>
2025-05-12 15:58:23 -0700

    add Designed Blocks

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // Known domain for project assets - ignore spell check warning
+    domains: ["res.cloudinary.com"],
+  },
 };
 
 export default nextConfig;
