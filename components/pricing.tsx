4ea73400032b42fe56e76d8ed5ef1245cc63d991
Francis Pham <francis.pham.ca@gmail.com>
2025-06-24 22:26:16 -0700

    fix pricing

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -10,7 +10,8 @@ const PricingSection = () => {
             Our Services & Pricing
           </h2>
           <p className="mt-4 text-gray-600">
-            Quality nail services at competitive prices
+            Quality nail services at competitive prices with 10 days guarantee
+            on all services
           </p>
         </div>
 
@@ -18,42 +19,29 @@ const PricingSection = () => {
           {/* Combo Services */}
           <Card className="p-6 shadow-lg rounded-lg">
             <h3 className="text-2xl font-bold  mt-4">Combo Services</h3>
-            <ul className="space-y-3 m-4">
+            <ul className="space-y-5 m-4">
               <li className="flex justify-between">
-                <span>R MP</span>
+                <span>Regular Manicure & Pedicure</span>
                 <span className="font-semibold">$57</span>
               </li>
-              <li className="text-sm text-gray-600 ml-4">Hand $22, Toe $33</li>
 
               <li className="flex justify-between">
-                <span>Deluxe MP</span>
-                <span className="font-semibold">$77</span>
+                <span>Regular Manicure & Shellac/Gel Pedicure</span>
+                <span className="font-semibold">$71</span>
               </li>
-              <li className="text-sm text-gray-600 ml-4">Hand $30, Toe $45</li>
 
               <li className="flex justify-between">
-                <span>RM GP</span>
-                <span className="font-semibold">$72</span>
-              </li>
-              <li className="text-sm text-gray-600 ml-4">Hand $23, Toe $47</li>
-
-              <li className="flex justify-between">
-                <span>GM RP</span>
+                <span>Shellac/Gel Manicure & Regular Pedicure</span>
                 <span className="font-semibold">$68</span>
               </li>
-              <li className="text-sm text-gray-600 ml-4">Hand $32, Toe $34</li>
 
               <li className="flex justify-between">
-                <span>G MP</span>
+                <span>Shellac/Gel Manicure & Pedicure</span>
                 <span className="font-semibold">$81</span>
               </li>
-              <li className="text-sm text-gray-600 ml-4">Hand $32, Toe $47</li>
-
-              <li className="flex justify-between">
-                <span>Deluxe G MP</span>
-                <span className="font-semibold">$101</span>
+              <li className="text-sm text-gray-600">
+                **Note: Additional $5 for gel removal
               </li>
-              <li className="text-sm text-gray-600 ml-4">Hand $40, Toe $59</li>
             </ul>
           </Card>
 
@@ -67,43 +55,28 @@ const PricingSection = () => {
               </li>
 
               <li className="flex justify-between">
-                <span>Acrylic OmBre Set</span>
-                <span className="font-semibold">$71</span>
-              </li>
-
-              <li className="flex justify-between">
                 <span>Acrylic Fill</span>
                 <span className="font-semibold">$51</span>
               </li>
 
               <li className="flex justify-between">
-                <span>Acrylic Ombre Fill</span>
-                <span className="font-semibold">$61</span>
-              </li>
-
-              <li className="flex justify-between">
-                <span>Hard Gel/BIAB Set (short and Medium)</span>
+                <span>Hard Gel Set (short and Medium)</span>
                 <span className="font-semibold">$66</span>
               </li>
 
               <li className="flex justify-between">
-                <span>Hard Gel/BIAB Fill</span>
+                <span>Hard Gel Refill</span>
                 <span className="font-semibold">$56</span>
               </li>
 
               <li className="flex justify-between">
-                <span>Overlay/Gel Builder</span>
-                <span className="font-semibold">$46</span>
+                <span>Biab Gel</span>
+                <span className="font-semibold">$56</span>
               </li>
 
               <li className="flex justify-between">
                 <span>GelX Set</span>
                 <span className="font-semibold">$76</span>
-              </li>
-
-              <li className="flex justify-between">
-                <span>Color Change (short and Medium)</span>
-                <span className="font-semibold">$36</span>
               </li>
             </ul>
           </Card>
@@ -113,18 +86,17 @@ const PricingSection = () => {
             <h3 className="text-2xl font-bold mt-4">Add-ons</h3>
             <ul className="space-y-5 m-4">
               <li className="flex justify-between">
-                <span>Shellac Take off</span>
-                <span className="font-semibold">+$10</span>
+                <span>Fixing Nails</span>
+                <span className="font-semibold">+$5</span>
               </li>
 
               <li className="flex justify-between">
-                <span>Foreign Removal</span>
+                <span>Removal</span>
                 <span className="font-semibold">+$15</span>
               </li>
 
-              <li className="flex justify-between">
-                <span>Soak off removal + strengthening gel</span>
-                <span className="font-semibold">Included</span>
+              <li className="text-sm text-gray-600 ml-4">
+                <span>Includes a soak off removal + Nails Strengthening</span>
               </li>
 
               <li className="flex justify-between">
@@ -148,70 +120,153 @@ const PricingSection = () => {
               </li>
 
               <li className="flex justify-between">
-                <span>French</span>
+                <span>French Tip</span>
                 <span className="font-semibold">+$10</span>
-              </li>
-
-              <li className="flex justify-between">
-                <span>Paraffin</span>
-                <span className="font-semibold">$10</span>
               </li>
             </ul>
           </Card>
 
-          {/* Discounts & Basic Services */}
+          {/* Special Discounts */}
           <Card className="p-6 shadow-lg rounded-lg">
-            <h3 className="text-2xl font-bold mt-4">
-              Special Discounts & Basic Services
-            </h3>
+            <h3 className="text-2xl font-bold mt-4">Special Discounts</h3>
             <ul className="space-y-3 m-4">
-              <li className="font-medium mt-2">Kids & Basic Services</li>
-              <li className="flex justify-between">
-                <span>Kid under 10 years</span>
-                <span className="font-semibold">Special pricing</span>
-              </li>
+              <li className="font-medium">Kid Services (Kid under 10 years)</li>
 
-              <li className="flex justify-between">
-                <span>Manicure</span>
+              <li className="flex justify-between ml-4">
+                <span>Regular Manicure</span>
                 <span className="font-semibold">$11</span>
               </li>
-              <li className="text-sm text-gray-600 ml-4">with gel +$10</li>
+              <li className="text-sm text-gray-600 ml-8">
+                with Shellac/Gel +$10
+              </li>
 
-              <li className="flex justify-between">
-                <span>Pedicure</span>
+              <li className="flex justify-between ml-4">
+                <span>Regular Pedicure</span>
                 <span className="font-semibold">$21</span>
               </li>
-              <li className="text-sm text-gray-600 ml-4">with gel +$10</li>
+              <li className="text-sm text-gray-600 ml-8">
+                with Shellac/Gel +$10
+              </li>
 
-              <li className="font-medium mt-6">Discounts</li>
-              <li className="flex justify-between">
+              <li className="font-medium mt-4 flex justify-between">
                 <span>Under 18 years</span>
-                <span className="font-semibold">10%-15% off all services</span>
+                <span>10%-15% off all services</span>
               </li>
 
-              <li className="font-medium mt-4">
-                Group Party (group of 5 people)
+              <li className="font-medium mt-4">Group Party</li>
+              <li className="ml-4">
+                Booking for 5 or more guests and enjoy FREE special drink and
+                snack on us
               </li>
-              <li className="ml-4">- Free drinks and snacks</li>
 
-              <li className="flex justify-between">
-                <span>5 times</span>
+              <li className="font-medium mt-4">Exclusive Reward</li>
+              <li className="flex justify-between ml-4">
+                <span>5 time visiting</span>
                 <span className="font-semibold">5% off</span>
               </li>
 
-              <li className="flex justify-between">
-                <span>Second coming</span>
+              <li className="flex justify-between ml-4">
+                <span>Second visiting</span>
                 <span className="font-semibold">10% off</span>
               </li>
 
-              <li className="flex justify-between">
+              <li className="flex justify-between ml-4">
                 <span>Birthday</span>
-                <span className="font-semibold">Special discount</span>
+                <span className="font-semibold">
+                  15% plus Special Gift (show ID)
+                </span>
+              </li>
+            </ul>
+          </Card>
+
+          {/* Waxing */}
+          <Card className="p-6 shadow-lg rounded-lg">
+            <h3 className="text-2xl font-bold mt-4">Waxing Services</h3>
+            <ul className="space-y-5 m-4">
+              <li className="flex justify-between">
+                <span>Full face</span>
+                <span className="font-semibold">$36</span>
               </li>
 
               <li className="flex justify-between">
-                <span>Grand opening</span>
-                <span className="font-semibold">15% off (for 2 weeks)</span>
+                <span>Eyebrows</span>
+                <span className="font-semibold">$15</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Mustache</span>
+                <span className="font-semibold">$10</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Forehead</span>
+                <span className="font-semibold">$15</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Sideburns</span>
+                <span className="font-semibold">$11</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Chin</span>
+                <span className="font-semibold">$10</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Under Arm</span>
+                <span className="font-semibold">$15</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Full Arm</span>
+                <span className="font-semibold">$36</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Half Arm</span>
+                <span className="font-semibold">$26</span>
+              </li>
+            </ul>
+          </Card>
+
+          {/* Waxing */}
+          <Card className="p-6 shadow-lg rounded-lg">
+            <h3 className="text-2xl font-bold mt-4">More Waxing Services</h3>
+            <ul className="space-y-5 m-4">
+              <li className="flex justify-between">
+                <span>Full Leg</span>
+                <span className="font-semibold">$61</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Half Leg</span>
+                <span className="font-semibold">$36</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Bikini</span>
+                <span className="font-semibold">$26</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Brazilian</span>
+                <span className="font-semibold">$46</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Chest</span>
+                <span className="font-semibold">$26</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Tummy</span>
+                <span className="font-semibold">$21</span>
+              </li>
+
+              <li className="flex justify-between">
+                <span>Back</span>
+                <span className="font-semibold">$46</span>
               </li>
             </ul>
           </Card>
