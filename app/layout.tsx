61d17745c5d97e3b466c86e2bfa009ccc5441726
Francis Pham <32145268+francispham@users.noreply.github.com>
2025-08-27 12:27:15 -0700

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -55,20 +56,15 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="Glossé Nails" />
         <link rel="apple-touch-icon" href="/apple-icon.png" type="image/png" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-P7PHDHXL');
-        </script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleTagManager
+          gtmId={process.env.NEXT_PUBLIC_GTM_ID || "GTM-YOUR_DEFAULT_ID"}
+        />
         <Analytics />
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7PHDHXL"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       </body>
     </html>
   );
